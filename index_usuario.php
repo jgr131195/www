09c3a061b7d48e7511a0602db4a5afa5cc9ffa9c
jgr131195 <ray131195@gmail.com>
2019-10-12 18:23:35 +0200

    commit

---
+++ index_usuario.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <div class="container-fluid">
+        <nav class="navbar navbar-inverse bg-inverse">
+        <!--
+            <a class="navbar-brand" href="#">
+                <img src="RUTA" width="30" height="30" class="d-inline-block align-top" alt=""> 
+            </a>
+         -->
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">algo</a>
+                <a class="nav-item nav-link" href="#">algo</a>
+                <a class="nav-item nav-link" href="#">algo</a>
+                <a class="nav-item nav-link" href="#">algo</a>
+            </div>
+        </nav>
+    </div>
+    
+
+    <div class="container">
+        <form action="">
+            <div class="form-group row">
+                <label for="" class="col-1 col-form-label">Nombre</label>
+                <input type="text" placeholder="escribe" name="nombre" class="form-control col-11">
+            </div>
+            <div class="form-group row">
+                <label for="" class="col-1 col-form-label">Apellidos</label>
+                <input type="text" placeholder="escribe" name="apellido" class="form-control col-11">
+            </div>
+            <div class="form-group row">
+                <label for="" class="col-1 col-form-label">Email</label>
+                <input type="text" placeholder="escribe" name="email" class="form-control col-11">
+            </div>
+        </form>
+    
+    </div>
+
+  </body>
+</html>
